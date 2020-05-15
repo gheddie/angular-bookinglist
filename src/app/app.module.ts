25ed0f2609c71885b48c00b9ff1ca64d65ab031a
gheddie <stefan.schulz.1976@googlemail.com>
2020-05-15 10:32:09 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+
+const routes: Routes = [
+  { path: 'first-component', component: BookingListComponent },
+  { path: '',   redirectTo: '/booking-list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, BookingListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
